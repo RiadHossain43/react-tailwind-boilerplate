82b6d2f7dc5aa7d21b15d668b955a612b3b277c8
RiadHossain43 <riadhossain7464@gmail.com>
2022-09-17 14:47:00 +0600

    init: Initialise the project

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./assets/css/index.css";
+const App = () => {
+  return <h1></h1>;
+};
+
+export default App;
---
+++ src/layouts/AuthLayout/Index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./assets/css/index.css";
+const App = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Welcome to React App thats build using Webpack and Babel separately
+    </h1>
+  );
+};
+
+export default App;
